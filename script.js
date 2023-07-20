da1af21c97c94193f239d07ad729fda2ea220d09
SoichiroIto123 <itosouchi1203@gmail.com>
2023-07-20 10:24:10 +0900

    変更

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 setTimeout(() => {
     const obj = document.querySelector('.scroll');
     obj.style.display = 'block';
-}, 6000);
+}, 6250);
+
+
+function closeMenu() {
+    // ハンバーガーメニューのチェックボックスを非選択状態にする
+    document.getElementById("menu-btn-check").checked = false;
+}
+document.addEventListener("click", function(event) {
+    // クリックされたリンクがページ内リンクかつハンバーガーメニューが開いている場合のみ、メニューを閉じる
+    if (event.target.tagName === "A" && event.target.getAttribute("href").charAt(0) === "#" && document.getElementById("menu-btn-check").checked) {
+      closeMenu();
+    }
+});
